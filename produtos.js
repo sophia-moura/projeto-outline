361651ab10285832df48191f74a74db7a2102b45
sarahpessoa843-stack <sarahpessoa843@gmail.com>
2026-09-24 07:23:16 -0300

    produtos.js

---
+++ produtos.js
@@ -0,0 +1,26 @@
+const produtos = [
+    {
+        nome: "Mochila Outline Classic",
+        preco: 299.90,
+        categoria: "mochilas",
+        imagem: "Mochila1.png"
+    },
+    {
+        nome: "Mochila Outline Color",
+        preco: 349.90,
+        categoria: "mochilas",
+        imagem: "Mochila2.jpg"
+    },
+    {
+        nome: "Mochila Outline Urban",
+        preco: 399.90,
+        categoria: "mochilas",
+        imagem: "mochila3.jpg"
+    },
+    {
+        nome: "Mochila Outline Mini",
+        preco: 249.90,
+        categoria: "mochilas",
+        imagem: "Mochila4.jpg"
+    }
+];
